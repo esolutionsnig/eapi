bf8d80ef265c57360a171c5442066a0c3b86a647
Ernest Ibeh <ibeh_ernest@yahoo.com>
2019-04-24 10:08:32 +0100

    Customized Exception Trait

---
+++ app/Exceptions/ExceptionTrait.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Exceptions;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+
+trait ExceptionTrait
+{
+    public function apiException($request,$e) {
+        if ($this->isModel($e)) {
+            return $this->ModelResponse($e);
+        }
+
+        if ($this->isHttp($e)) {
+            return $this->HttpReponse($e);
+        }
+
+        return parent::render($request, $exception);
+    }
+
+    protected function isModel($e) {
+        return $e instanceof ModelNotFoundException;
+    }
+
+    protected function isHttp($e) {
+        return $e instanceof NotFoundHttpException;
+    }
+
+    protected function ModelResponse($e) {
+        return response()->json([
+            'errors' => 'Product Model Not Found'
+        ],Response::HTTP_NOT_FOUND);
+    }
+
+    protected function HttpReponse($e) {
+        return response()->json([
+            'errors' => 'Incorrect Route'
+        ],Response::HTTP_NOT_FOUND);
+    }
+}
--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,13 +3,12 @@
 namespace App\Exceptions;
 
 use Exception;
-use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Exceptions\ExceptionTrait;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ExceptionTrait;
     /**
      * A list of the exception types that are not reported.
      *
@@ -50,20 +49,10 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         if ($request->expectsJson()) {
-            if ($exception instanceof ModelNotFoundException) {
-                return response()->json([
-                    'errors' => 'Product Model Not Found'
-                ],Response::HTTP_NOT_FOUND);
-            }
+            
+            return $this->apiException($request,$exception);
         }
 
-        if ($exception instanceof NotFoundHttpException) {
-            return response()->json([
-                'errors' => 'Incorrect Route'
-            ],Response::HTTP_NOT_FOUND);
-        }
-        // Die and Dump
-        // dd($exception);
         return parent::render($request, $exception);
     }
 }
